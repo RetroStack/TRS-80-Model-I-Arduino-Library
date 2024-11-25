6d4e24fb83d85b56be7b9b3f62d863e6d84b87b3
Marcel Erz <info@retrostack.org>
2024-11-24 18:29:20 -0800

    Remove unnecessary outputs

--- src/utils.cpp
+++ src/utils.cpp
@@ -68,17 +68,17 @@ void asmWait(uint16_t wait)
 void asmWait(uint16_t outerLoopCount, uint16_t innerLoopCount)
 {
   asm volatile(
-      "outer_loop_start: \n\t"                     // Outer loop start label
-      "movw r24, %A0 \n\t"                         // Copy outer loop count to r24:r25
-      "inner_loop_start: \n\t"                     // Inner loop start label
-      "movw r26, %A1 \n\t"                         // Copy inner loop count to r26:r27
-      "inner_loop: \n\t"                           // Inner loop label
-      "sbiw r26, 1 \n\t"                           // Subtract one from the inner loop count
-      "brne inner_loop \n\t"                       // Branch to Inner loop label if zero flag is clear
-      "sbiw r24, 1 \n\t"                           // Subtract one from the outer loop count
-      "brne inner_loop_start \n\t"                 // Branch to Inner loop start label if zero flag is clear
-      : "+w"(outerLoopCount), "+w"(innerLoopCount) // Outputs: modified in place
-      : "r"(outerLoopCount), "r"(innerLoopCount)   // Inputs
-      : "r24", "r25", "r26", "r27"                 // Clobbers
+      "outer_loop_start: \n\t"                   // Outer loop start label
+      "movw r24, %A0 \n\t"                       // Copy outer loop count to r24:r25
+      "inner_loop_start: \n\t"                   // Inner loop start label
+      "movw r26, %A1 \n\t"                       // Copy inner loop count to r26:r27
+      "inner_loop: \n\t"                         // Inner loop label
+      "sbiw r26, 1 \n\t"                         // Subtract one from the inner loop count
+      "brne inner_loop \n\t"                     // Branch to Inner loop label if zero flag is clear
+      "sbiw r24, 1 \n\t"                         // Subtract one from the outer loop count
+      "brne inner_loop_start \n\t"               // Branch to Inner loop start label if zero flag is clear
+      :                                          //"+w"(outerLoopCount), "+w"(innerLoopCount) // Outputs: modified in place
+      : "r"(outerLoopCount), "r"(innerLoopCount) // Inputs
+      : "r24", "r25", "r26", "r27"               // Clobbers
   );
 }
